591dd616f4c855b9767debd5c3f307748d75b67b
FelipeVegners <felipevegners@gmail.com>
2019-08-16 17:06:51 -0300

    adjust list to be droppable

--- src/components/Card/index.js
+++ src/components/Card/index.js
@@ -6,21 +6,25 @@ import BoardContext from '../Board/context'
 import { Container, Label } from './styles'
 
 export default function Card({ data, index, listIndex }) {
-const ref = useRef()
-const { move } = useContext(BoardContext)
-
+  const ref = useRef()
+  const { move } = useContext(BoardContext)
+  let memTime = 0
   const [{ isDragging }, dragRef] = useDrag({
     item: { type: 'CARD', index, listIndex },
     collect: monitor => ({
       isDragging: monitor.isDragging(),
-    }),    
+    }),
   })
 
   const [, dropRef] = useDrop({
     accept: 'CARD',
     hover(item, monitor) {
+      var date = new Date()
+      var now = date.getTime()
+      if(memTime == 0) memTime = now
       const draggedListIndex = item.listIndex
       const targetListIndex = listIndex
+
       const draggedIndex = item.index
       const targetIndex = index
 
@@ -30,36 +34,34 @@ const { move } = useContext(BoardContext)
 
       const targetSize = ref.current.getBoundingClientRect()
       const targetCenter = (targetSize.bottom - targetSize.top) / 2
-      
+
       const draggedOffset = monitor.getClientOffset()
       const draggedTop = draggedOffset.y - targetSize.top
 
-      if(draggedIndex < targetIndex && draggedTop < targetCenter) {
+      if (draggedIndex < targetIndex && draggedTop < targetCenter) {
         return
       }
-
-      if(draggedIndex > targetIndex && draggedTop > targetCenter) {
+      if (draggedIndex > targetIndex && draggedTop > targetCenter) {
         return
       }
-
-      move(draggedListIndex, targetListIndex, draggedIndex, targetIndex)
-
-      item.index = targetIndex
-      item.listIndex = targetListIndex
+      if((now - memTime) >= 300 || draggedListIndex == targetListIndex){
+        move(draggedListIndex, targetListIndex, draggedIndex, targetIndex,'card')
+        item.index = targetIndex
+        item.listIndex = targetListIndex
+        memTime = 0
+      }
     }
   })
 
   dragRef(dropRef(ref))
 
   return (
-    <Container ref={ ref } isDragging={ isDragging }>
+    <Container ref={ref} isDragging={isDragging}>
       <header>
-      { data.labels.map(label => <Label color={ label } key={ label }/>) }
-        
+        {data.labels.map(label => <Label key={label} color={label} />)}
       </header>
-      <p>{ data.content }</p>
-      { data.user && <img src={ data.user } alt=""/> }
-      
+      <p>{data.content}</p>
+      { data.user && <img src={data.user} alt=""/> }
     </Container>
   )
 }
